dfa82cdcd838f4694e28ff96d9c05e7c0d47321b
TaKawwa <mikolaj.dudek@onet.pl>
2025-11-02 18:15:03 +0000

    Changed the formatting of most of the variables and functions to actually follow the coding style guide.
    Fixed the "columnVariables" so it contains all the variables and is usable in different files.
    
    Fixed the redundancies in most files by importing the dataset from the "importDataset.py".
    
    Added sorting by different variables.

---
+++ importDataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# File for importing the csv into a more advanced 2D matrix 
+# data_set -> The dataset inside the 2D matrix.
+
+
+def loadData():
+    try:
+        data_set = pd.read_csv("SpotifyFeatures.csv", skiprows=1)
+        print("Data imported successfully:")
+        return data_set
+    
+    except Exception as e:
+        print("Error while importing CSV:")
+        print(e)
+    
+
+if __name__ == "__main__":
+    # Just to test
+    data_set = loadData()
+    print(data_set.head())
