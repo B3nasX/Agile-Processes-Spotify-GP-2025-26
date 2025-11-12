1050e80f43ac25c9a96bbce7ca92dc524e3f42f7
Emily Beamish <emilybeamish2020@gmail.com>
2025-11-12 22:16:45 +0000

    bar charts for tempo, popularity

--- tempoVisualization.py
+++ tempoVisualization.py
@@ -2,15 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-sample_data = {
-    'tempo': [120, 130, 125, 140, 150, 160, 170, 180, 190, 200,]
+df = pd.read_csv('SpotifyFeatures.csv')
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
+genre_tempo = df.groupby('genre')['tempo'].mean().sort_values(ascending=False)
+genre_tempo.plot(kind='bar', color='skyblue', ax=ax1)
+ax1.set_title('Average Tempo by Genre')
+ax1.set_xlabel('Genre')
+ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45, fontsize=8)
 
 
-}
-df = pd.DataFrame(sample_data)
-plt.figure(figsize=(10, 6))
-plt.plot(df.index, df['tempo'], marker='o', linestyle='-', color='blue')
-plt.title('Tempo Visualization Over Time')
-plt.xlabel('Time')
-plt.ylabel('Tempo (BPM)')
+genre_popularity = df.groupby('genre')['popularity'].mean().sort_values(ascending=False)
+genre_popularity.plot(kind='bar', color='salmon', ax=ax2)
+ax2.set_title('Average Popularity by Genre')
+ax2.set_xlabel('Genre')
+ax2.set_xticklabels(ax2.get_xticklabels(), rotation=45, fontsize=8)
+
+
+plt.tight_layout()
 plt.show()
